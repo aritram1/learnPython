dbedd732eed55ca3429ae842deeae64184315ae5
aritram1 <aritram1@gmail.com>
2019-07-17 02:14:14 +0530

    OrderedDict examples are added
    A dict is generated from the names of days like 'Mon: 'Monday' etc. OrderedDict is used where order is required to retain.

--- 09 - dicts.py
+++ 09 - dicts.py
@@ -1,1 +1,22 @@
-# dict examples
+# dict and orderedDict examples
+
+from collections import OrderedDict 
+
+# Data
+DAYS = ['Sunday', 'Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday','Saturday']
+
+# Make and display the DAYS in dict (order is NOT retained)
+DAYSdict = {day[0:3]:day for day in DAYS}
+for day in DAYSdict:
+    print('{0} = {1}'.format(day, DAYSdict[day]))
+
+print('-'*20)
+# Make and display the DAYS in Ordereddict (order IS retained)
+DAYSOrdereddict = OrderedDict()
+for day in DAYS:
+    shortname = day[0:3]
+    DAYSOrdereddict[shortname] = day
+for day in DAYSOrdereddict:
+    print('{0} = {1}'.format(day, DAYSOrdereddict[day]))
+    
+    
