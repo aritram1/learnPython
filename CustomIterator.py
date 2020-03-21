ea58502d809fa243bb493ab9470feba88be0ed99
aritram1 <aritram1@gmail.com>
2020-03-22 03:20:23 +0530

    Update CustomIterator.py

--- CustomIterator.py
+++ CustomIterator.py
@@ -9,11 +9,11 @@ class myiterable:
         self.iterator = {}
 
     def add(self, item):
-        self.content['myiterable' + str(myiterable.count)] = item
+        self.content[myiterable.count] = item
         myiterable.count= myiterable.count + 1
 
     def __iter__(self):
-        each = self.content['myiterable' + str(myiterable.current_it_count)]
+        each = self.content[myiterable.current_it_count]
         key = list(each.keys())[0];
         self.iterator = {
             'key' : key,
@@ -24,34 +24,42 @@ class myiterable:
     def __next__(self):
         if myiterable.current_it_count >= myiterable.count:
             raise StopIteration();
-        result = self.content['myiterable'+str(myiterable.current_it_count)]
+        result = self.content[myiterable.current_it_count]
         myiterable.current_it_count = myiterable.current_it_count + 1
         return result;
 
 # Lets test 
-aritra = myiterable()
-
-# Add some properties
-aritra.add({'name' : 'aritra'})
-aritra.add({'roll' : {'a' : '123'}})
-aritra.add({'one' : {'two' : 'three'}})
-aritra.add({'otherValues' : {'somemore' : {'someother' : 'finallValue'}}})
-aritraIterator = iter(aritra)
-
 # Usage
+# aritra = myiterable()
 # print(next(aritraIterator)) #{'name' : 'aritra'}
 # print(next(aritraIterator)) #{'roll' : 123}
 # print(next(aritraIterator)) # creates StopIteration exception [hence commented]
 
+aritra = myiterable()
+
+# Add some properties
+aritra.add({'name' : 'aritra'})
+aritra.add({'roll' : {'a' : 123, 'b': True}})
+aritra.add({1 : {'two' : 'three'}})
+aritra.add({'otherValues' : {'somemore' : {456 : 'finallValue'}}})
+# aritra -> {
+# 0: {'name': 'aritra'}, 
+# 1: {'roll': {'b': True, 'a': 123}}, 
+# 2: {1: {'two': 'three'}}, 
+# 3: {'otherValues': {'somemore': {456: 'finallValue'}}}}
+
+aritraIterator = iter(aritra)
+
+# Iterate the iterable and produce values. The values are lazy evaluated.
 while(True):
-try:
-    print(next(aritraIterator))
-except(StopIteration):
-    print('All values printed')
-    break
+    try:
+        print(next(aritraIterator))
+    except(StopIteration):
+        print('All values printed')
+        break
 
 # output:
-# {'roll': {'a': '123'}}                                                                                                                 
-#{'one': {'two': 'three'}}                                                                                                              
-# {'otherValues': {'somemore': {'someother': 'finallValue'}}}                                                                            
-# All values printed 
+# {'roll': {'b': True, 'a': 123}}                                                                                                        
+# {1: {'two': 'three'}}                                                                                                                  
+# {'otherValues': {'somemore': {456: 'finallValue'}}}                                                                                    
+# All values printed  
