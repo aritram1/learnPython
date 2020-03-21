91083a6ea6aaa58efa782f83ad9b1d887dd365d1
aritram1 <aritram1@gmail.com>
2020-03-22 03:08:57 +0530

    Update CustomIterator.py
    format changed. Output added.

--- CustomIterator.py
+++ CustomIterator.py
@@ -1,17 +1,17 @@
-# A collection type class that can iterate with custom defined iterator            
+# A collection type class that can iterate with custom iterator            
 
 class myiterable:
     count = 0
     current_it_count = 0;
-    
+
     def __init__(self):
         self.content = {}
         self.iterator = {}
-    
+
     def add(self, item):
         self.content['myiterable' + str(myiterable.count)] = item
         myiterable.count= myiterable.count + 1
-    
+
     def __iter__(self):
         each = self.content['myiterable' + str(myiterable.current_it_count)]
         key = list(each.keys())[0];
@@ -20,7 +20,7 @@ class myiterable:
             'value' : each[key]
         }
         return self
-        
+
     def __next__(self):
         if myiterable.current_it_count >= myiterable.count:
             raise StopIteration();
@@ -37,17 +37,21 @@ aritra.add({'roll' : {'a' : '123'}})
 aritra.add({'one' : {'two' : 'three'}})
 aritra.add({'otherValues' : {'somemore' : {'someother' : 'finallValue'}}})
 aritraIterator = iter(aritra)
-display(aritraIterator);
 
 # Usage
 # print(next(aritraIterator)) #{'name' : 'aritra'}
 # print(next(aritraIterator)) #{'roll' : 123}
 # print(next(aritraIterator)) # creates StopIteration exception [hence commented]
 
-def display:
-    while(True):
-        try:
-            print(next(aritraIterator))
-        except(StopIteration):
-            print('All values printed')
-            break
+while(True):
+try:
+    print(next(aritraIterator))
+except(StopIteration):
+    print('All values printed')
+    break
+
+# output:
+# {'roll': {'a': '123'}}                                                                                                                 
+#{'one': {'two': 'three'}}                                                                                                              
+# {'otherValues': {'somemore': {'someother': 'finallValue'}}}                                                                            
+# All values printed 
